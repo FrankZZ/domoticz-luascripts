3dc1ee6fe3d67df9f926445f052b239bcf6c9a51
FrankZZ <frank@wamm.es>
2017-04-24 23:26:16 +0200

    Fix for forcelight, substring was 1 character off

--- script_device_forcelight.lua
+++ script_device_forcelight.lua
@@ -1,7 +1,7 @@
 commandArray = {}
 for deviceName,deviceValue in pairs(devicechanged) do
     type = deviceName:sub(-5)
-    name = deviceName:sub(1,-5)
+    name = deviceName:sub(1,-6)
     if (type == 'Force') then
         if(otherdevices[name .. 'Force' == 'On']) then
             print ('[' .. name .. '] Forcing on...')
