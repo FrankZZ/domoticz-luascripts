0fa33e33ddbbb1e32e42e4ecc54f5be7ad513efb
FrankZZ <frank@wamm.es>
2017-04-24 23:30:09 +0200

    Amend

--- script_device_forcelight.lua
+++ script_device_forcelight.lua
@@ -2,11 +2,12 @@ commandArray = {}
 for deviceName,deviceValue in pairs(devicechanged) do
     type = deviceName:sub(-5)
     name = deviceName:sub(1,-6)
+    
     if (type == 'Force') then
-        if(otherdevices[name .. 'Force' == 'On']) then
+        if(deviceValue == 'On') then
             print ('[' .. name .. '] Forcing on...')
             commandArray['Group:' .. name .. 'Regular'] = 'On'
-        elseif(otherdevices[name .. 'Force' == 'Off']) then
+        elseif(deviceValue == 'Off') then
             print ('[' .. name .. '] Forcing off...')
             commandArray['Group:' .. name .. 'Regular'] = 'Off'
         end
