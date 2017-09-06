25553f848a5c6e6c5296e5a9bb923c192ea09836
FrankZZ <frank@wamm.es>
2017-09-06 11:56:50 +0200

    Debug variables

--- script_time_lightintensity.lua
+++ script_time_lightintensity.lua
@@ -11,6 +11,20 @@ sunisdark = tonumber(sUV) < uservariables['minSunlight']
 
 --sleeptime = true
 
+function LogVariables(x,depth,name)
+    for k,v in pairs(x) do
+        if (depth>0) or ((string.find(k,'device')~=nil) or (string.find(k,'variable')~=nil) or
+                         (string.sub(k,1,4)=='time') or (string.sub(k,1,8)=='security')) then
+            if type(v)=="string" then print(name.."['"..k.."'] = '"..v.."'") end
+            if type(v)=="number" then print(name.."['"..k.."'] = "..v) end
+            if type(v)=="boolean" then print(name.."['"..k.."'] = "..tostring(v)) end
+            if type(v)=="table" then LogVariables(v,depth+1,k); end
+        end
+    end
+end
+-- call with this command
+LogVariables(_G,0,'');
+
 commandArray = {}
 for deviceName,deviceValue in pairs(otherdevices) do
     type = deviceName:sub(0,3)
